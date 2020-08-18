87c53441f830099cc41d57d4ecd173151d8fedd6
vagrant <vagrant@localhost.localdomain>
2020-08-18 07:11:47 +0000

    add-home-about

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  get 'home/about' => 'homes#about', as:'about'
+  resources :posts do
+  	resources :comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
+  resources :users do
+  get 'follows' => 'relationships#follows'
+  get 'followers' => 'relationships#followers'
+  end
+  resources :relationships, only: [:create, :destroy]
 end
