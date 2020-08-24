00e2e751f7eb6d9f86aaa2604f6974a05606e4cc
vagrant <vagrant@localhost.localdomain>
2020-08-24 07:03:35 +0000

    add-follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,41 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user, only: [:create, :destroy]
+
+  def follows
+    @user = User.find(params[:user_id])
+    @users = @user.followings
+  end
+
+  def followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers
+  end
+
   def create
+    following = current_user.follow(@user)
+    if following.save
+      flash[:success] = "ユーザーをフォローしました"
+      redirect_back(fallback_location: user_path(@user))
+    else
+      flash.now[:alert] = "ユーザーのフォローに失敗しました"
+      redirect_back(fallback_location: user_path(@user))
+    end
   end
 
   def destroy
+    following = current_user.unfollow(@user)
+    if following.destroy
+      flash[:success] = "ユーザーのフォローを解除しました"
+      redirect_back(fallback_location: user_path(@user))
+    else
+      flash.now[:alert] = "ユーザーのフォロー解除に失敗しました"
+      redirect_back(fallback_location: user_path(@user))
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:relationship][:follow_id])
   end
 end
+
